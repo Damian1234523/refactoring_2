5d67161d620dd07d1376336f48fd378a3004b63f
Damian1234523 <dpistorius@posteo.de>
2017-05-09 12:23:21 +0200

    line was too long
    excluded ProgramTest form compiling

--- src/Customer.java
+++ src/Customer.java
@@ -36,7 +36,8 @@ class Customer {
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
                 frequentRenterPoints ++;
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
+            result += "\t" + each.getMovie().getTitle()+ "\t" +
+                    "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
         }
         //add footer lines
